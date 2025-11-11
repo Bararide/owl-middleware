b5f7e83d2b1cd0aaa2e7777db89419e5fed7bf0d
Bararide <445467246r@gmail.com>
2025-11-12 00:00:09 +0300

    added http user resolver

--- owl_middleware/resolvers/user.py
+++ owl_middleware/resolvers/user.py
@@ -11,3 +11,13 @@ async def resolve_user(
     event: Union[Message, CallbackQuery], auth_service: AuthService
 ) -> Result[User, Exception]:
     return await auth_service.get_user(event.from_user.id)
+
+
+async def resolve_user_http(
+    request, auth_service: AuthService
+) -> Result[User, Exception]:
+    user_id = request.headers.get("X-User-ID")
+    if not user_id:
+        return Result.Err(Exception("Not authenticated"))
+
+    return await auth_service.get_user_by_id(user_id)
