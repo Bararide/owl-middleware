b6081594a038abfd4ee7fe83528f214cacc4ec0b
Bararide <445467246r@gmail.com>
2025-11-30 13:30:40 +0300

    fix generate_response

--- owl_middleware/services/agent.py
+++ owl_middleware/services/agent.py
@@ -135,6 +135,9 @@ class AgentService:
                 prompt = self._get_cached_prompt(prompt_type, **kwargs)
                 prompt_text = prompt.format(**context)
 
+                if isinstance(prompt_text, BasePrompt):
+                    prompt_text = prompt_text.config.template
+
             response = await self._llm_model.generate(prompt_text)
 
             return Ok(
